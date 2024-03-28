1a6638dfb1bb67766668b3a17fd01560b5e90fab
Tess McGovern <ren.mcgovern@gmail.com>
2024-03-27 21:08:18 -0400

    proj proposal, read me

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Get the #1 song for a given day
+GET /:1991/:june/:19
+
+// Returns:
+{
+  "name": "Name of hit song",
+  "artist": "Name of Artist",
+  "startDate": "(first day on chart, in UTC)",
+  "endDate": "(last day on chart, in UTC)"
+}
