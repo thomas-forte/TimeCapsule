ec3ae327cfaa827e26c7653540944fbc1e38e1ca
BreTre <bctrepal@gmail.com>
2024-04-01 19:35:44 -0400

    added hex color codes and fonts to js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,83 +3,83 @@ const cardContainer = document.getElementById('#card-container');
 // Colors and font selection
 function renderStyles() {
     if (year >= 1950 && year < 1960) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});
+        $(':root').css({'--primary-color': '#3EADD2', 
+                        '--secondary-color': '#F2CB05', 
+                        '--accent-color': '#F28157',
+                        '--text-color': '#031C26',
+                        '--background-color': '#FFF7EF',
+                        '--heading': 'Carter One',
+                        '--body': 'Truculenta'});
     }
 
     if (year >= 1960 && year < 1970) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});
+        $(':root').css({'--primary-color': '#FF8636', 
+                        '--secondary-color': '#54BCD6', 
+                        '--accent-color': '#9BBD25',
+                        '--text-color': '#310C0B',
+                        '--background-color': '#FFE101',
+                        '--heading': 'Spicy Rice',
+                        '--body': 'Life Savers'});
     }
 
     if (year >= 1970 && year < 1980) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});
+        $(':root').css({'--primary-color': '#E59F23', 
+                        '--secondary-color': '#7E9636', 
+                        '--accent-color': '#633B07',
+                        '--text-color': '#3C2000',
+                        '--background-color': '#E2C6A7',
+                        '--heading': 'Bungee Shade',
+                        '--body': 'Michroma'});
     }
 
     if (year >= 1980 && year < 1990) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});
+        $(':root').css({'--primary-color': '#FA8057', 
+                        '--secondary-color': '#E754B0', 
+                        '--accent-color': '#F3D751',
+                        '--text-color': '#8CA9FD',
+                        '--background-color': '#2B0D29',
+                        '--heading': 'Monoton',
+                        '--body': 'Orbitron'});
     }
 
     if (year >= 1990 && year < 2000) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});    
+        $(':root').css({'--primary-color': '#5ABFAD', 
+                        '--secondary-color': '#D93682', 
+                        '--accent-color': '#5D3B8C',
+                        '--text-color': '#FDB64C',
+                        '--background-color': '#161616',
+                        '--heading': 'Yuji Boku',
+                        '--body': 'Special Elite'});    
     }
 
     if (year >= 2000 && year < 2010) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});    
+        $(':root').css({'--primary-color': '#5E88BA', 
+                        '--secondary-color': '#B4CBE4', 
+                        '--accent-color': '#D6D6D6',
+                        '--text-color': '#112236',
+                        '--background-color': '#FCFBFC',
+                        '--heading': 'Cute Font',
+                        '--body': 'Jura'});    
     }
 
     if (year >= 2010 && year < 2020) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});    
+        $(':root').css({'--primary-color': '#', 
+                        '--secondary-color': '#', 
+                        '--accent-color': '#',
+                        '--text-color': '#',
+                        '--background-color': '#',
+                        '--heading': '',
+                        '--body': ''});    
     }
 
     if (year >= 2020) {
-        $(':root').css({'--primary-color': '#F8F8F8', 
-                        '--secondary-color': '#080808', 
-                        '--accent-color': '#D0D0D0',
-                        '--text-color': '#D0D0D0',
-                        '--background-color': '#D0D0D0',
-                        '--heading': '#D0D0D0',
-                        '--body': '#A9A9A9'});    
+        $(':root').css({'--primary-color': '#', 
+                        '--secondary-color': '#', 
+                        '--accent-color': '#',
+                        '--text-color': '#',
+                        '--background-color': '#',
+                        '--heading': '',
+                        '--body': ''});    
     }
 };
 
