09205595f5236813660b06792a1fb06d8f43e9a2
Tess McGovern <ren.mcgovern@gmail.com>
2024-04-01 21:27:23 -0400

    datepicker closes on submit and cancel now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,85 @@
-const cardContainer = document.getElementById('#card-container')
- let Movie = {
-     title: ,
-     description:
- }
-     let Book = {
-     title: ,
-     description:
- }
-     let News = 
+// const cardContainer = document.getElementById('#card-container')
+//  let Movie = {
+//      title: ,
+//      description:
+//  }
+//      let Book = {
+//      title: ,
+//      description:
+//  }
+//      let News = 
     
 
- Get the #1 song for a given day
- GET /:1991/:june/:19
+//  Get the #1 song for a given day
+//  GET /:1991/:june/:19
 
- // Returns:
- {
-   "name": "Name of hit song",
-  "artist": "Name of Artist",
-  "startDate": "(first day on chart, in UTC)",
-  "endDate": "(last day on chart, in UTC)"
- }
+//  // Returns:
+//  {
+//    "name": "Name of hit song",
+//   "artist": "Name of Artist",
+//   "startDate": "(first day on chart, in UTC)",
+//   "endDate": "(last day on chart, in UTC)"
+//  }
 
-function dynamicColors() { // Returns a date-based color scheme
-  element.style.color = "";
-}
-function changeColor() {
-var gfg = document.getElementById("gfg");
-gfg.style.color = "green";
-}
+// function dynamicColors() { // Returns a date-based color scheme
+//   element.style.color = "";
+// }
+// function changeColor() {
+// var gfg = document.getElementById("gfg");
+// gfg.style.color = "green";
+// }
 
-Articles API
-https://api.nytimes.com/svc/archive/v1/{year}/{month}.json
+// Articles API
+// https://api.nytimes.com/svc/archive/v1/{year}/{month}.json
 
-articleRequestURL = "https://api.nytimes.com/svc/archive/v1/{year}/{month}.json"
-bookRequestURL = "https://api.nytimes.com/svc/books/v3/lists/overview.json"
-const nytimesAPIkey = anAU8Yk0RQpGTel7ZLCurFyigefJRTo3
+// articleRequestURL = "https://api.nytimes.com/svc/archive/v1/{year}/{month}.json"
+// bookRequestURL = "https://api.nytimes.com/svc/books/v3/lists/overview.json"
+// const nytimesAPIkey = anAU8Yk0RQpGTel7ZLCurFyigefJRTo3
 
-fetch(articleRequestURL) {
-  const articles = await fetch ("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3")
+// fetch(articleRequestURL) {
+//   const articles = await fetch ("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3")
 
-};
+// };
 
-fetch(bookRequestURL) {
- const books = await fetch ("https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3")
-}
+// fetch(bookRequestURL) {
+//  const books = await fetch ("https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3")
+// }
 
-function createMovieCard(Movie) {
-    const movieCard = $("<div>").addclass("movieCard");
-    const movieCardBody = $("<div>").addClass("card-body");
-    const movieTitle = $("<h4>").addClass("movieTitle").text(Movie.title);
-    movieCardBody.append(movieTitle);
-    const movieDescription = $("<p>").addClass("movieDesc").text(Movie.description);
-    movieCardBody.append(movieDescription);
-    movieCard.append(movieCardBody);
-    cardContainer.append(movieCard);
-    return movieCard;
-}
-
-
-function createBookCard(Book) {
-    const bookCard = $("<div>").addclass("bookCard");
-    const bookCardBody = $("<div>").addClass("card-body");
-    const bookTitle = $("<h4>").addClass("bookTitle").text(Book.title);
-    bookCardBody.append(bookTitle);
-    const bookDescription = $("<p>").addClass("bookDesc").text(Book.description);
-    bookCardBody.append(bookDescription);
-    bookCard.append(bookCardBody);
-    cardContainer.append(bookCard);
-    return bookCard;
-}
+// function createMovieCard(Movie) {
+//     const movieCard = $("<div>").addclass("movieCard");
+//     const movieCardBody = $("<div>").addClass("card-body");
+//     const movieTitle = $("<h4>").addClass("movieTitle").text(Movie.title);
+//     movieCardBody.append(movieTitle);
+//     const movieDescription = $("<p>").addClass("movieDesc").text(Movie.description);
+//     movieCardBody.append(movieDescription);
+//     movieCard.append(movieCardBody);
+//     cardContainer.append(movieCard);
+//     return movieCard;
+// }
 
 
-function createNewsCard(News) {
-    const newsCard = $("<div>").addclass("newsCard");
-    const newsCardBody = $("<div>").addClass("card-body");
-    const newsTitle = $("<h4>").addClass("newsTitle").text(News);
-    newsCardBody.append(newsTitle);
-    newsCard.append(newsCardBody);
-    cardContainer.append(newsCard);
-    return newsCard;
-}
+// function createBookCard(Book) {
+//     const bookCard = $("<div>").addclass("bookCard");
+//     const bookCardBody = $("<div>").addClass("card-body");
+//     const bookTitle = $("<h4>").addClass("bookTitle").text(Book.title);
+//     bookCardBody.append(bookTitle);
+//     const bookDescription = $("<p>").addClass("bookDesc").text(Book.description);
+//     bookCardBody.append(bookDescription);
+//     bookCard.append(bookCardBody);
+//     cardContainer.append(bookCard);
+//     return bookCard;
+// }
+
+
+// function createNewsCard(News) {
+//     const newsCard = $("<div>").addclass("newsCard");
+//     const newsCardBody = $("<div>").addClass("card-body");
+//     const newsTitle = $("<h4>").addClass("newsTitle").text(News);
+//     newsCardBody.append(newsTitle);
+//     newsCard.append(newsCardBody);
+//     cardContainer.append(newsCard);
+//     return newsCard;
+// }
 
 
 
@@ -128,10 +128,6 @@ const handleFormSubmit = function (event) {
     var firstGetItem = JSON.parse(localStorage.getItem('birthdates'));
     if (!Array.isArray(firstGetItem))  {
         firstGetItem = [];
-
-
-    
-
     }
 
     const dateInput = dateInputEl.value;
@@ -139,22 +135,35 @@ const handleFormSubmit = function (event) {
     firstGetItem.push(dateInput);
     localStorage.setItem('birthdates', JSON.stringify(firstGetItem));
     return;
-
-   
-    }
     
-    formEl.addEventListener('submit', handleFormSubmit);  
+    }
 
- 
+// Closes Modal on Submit
+submitButton.addEventListener('click', function(){
+    document.querySelector('.modal').classList.remove('is-active');
+
+});
+
+    
+formEl.addEventListener('submit', handleFormSubmit);  
 
 
+//Closes Modal on Cancel
+const cancelButton = document.querySelector('#cancel-button');
+cancelButton.addEventListener('click', function(){
+    document.querySelector('.modal').classList.remove('is-active'); 
+
+});
 
 $(function () {
     $('#datepicker').datepicker({
       changeMonth: true,
       changeYear: true,
+      minDate: 0,
     });
   });
+
+  
 
 
 // To close modal by means of the X in upper-right corner
