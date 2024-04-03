a8c23d062549ed47ba44e09262c774d434e272e0
rvanetta97 <rachel.vanetta@gmail.com>
2024-04-03 18:36:14 -0400

    API fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,13 +89,19 @@ function dynamicColors() { // Returns a date-based color scheme
 
 }
 //API URLs
-articleRequestURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?pub_date=${birthday}&api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3"
-bookRequestURL = "https://api.nytimes.com/svc/books/v3/lists/overview.json?bestsellers_date=${birthday}&api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3"
-movieRequestURL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=${birthyear}&sort_by=revenue.desc"
+articleRequestURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?pub_date=${dateInput}&api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3`
+bookRequestURL = `https://api.nytimes.com/svc/books/v3/lists/overview.json?bestsellers_date=${dateInput}&api-key=anAU8Yk0RQpGTel7ZLCurFyigefJRTo3`
+movieRequestURL = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=${dateInput}&sort_by=revenue.desc`
 //Movie API Key
 movieAPIKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjYyNjI4N2M2ZGU3Yzk4ODUwNjVlNTBjN2MyNzY1NCIsInN1YiI6IjY2MDYxN2JmYTg5NGQ2MDE3YzYyNDkxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B1Lq6M9BipX3yYlMkNkluP9ljFAU7rklKjW2B6_X0_A"
 
-
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjYyNjI4N2M2ZGU3Yzk4ODUwNjVlNTBjN2MyNzY1NCIsInN1YiI6IjY2MDYxN2JmYTg5NGQ2MDE3YzYyNDkxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B1Lq6M9BipX3yYlMkNkluP9ljFAU7rklKjW2B6_X0_A'
+  }
+};
 //once birthday is submitted this function will run
 async function birthdaySubmission() {
   fetch(articleRequestURL)
@@ -112,7 +118,7 @@ async function birthdaySubmission() {
       const books = data.list
       console.log(data.list)
     });
-  fetch(moviesRequestURL, movieAPIKey)
+  fetch(moviesRequestURL, options)
     .then(response => response.json())
     .then(data => {
       const movies = data.list
