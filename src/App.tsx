eee55eae41b8085eaae798c489c384849cecd474
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-07-15 00:25:37 -0400

    self containing header logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,19 @@ import { Header } from "./components/Header";
 import { DateDialog } from "./components/DateDialog";
 
 export const App = () => {
-  const [showModal, setShowModal] = useState(false);
   const [dateEntered, setDateEntered] = useState<Date | null>(
-    new Date(1952, 9, 8)
+    new Date(1950, 9, 8)
   );
 
-  function openModal() {
-    setShowModal(true);
-  }
-
-  function closeModal() {
-    setShowModal(false);
-  }
-
-  function closeModalWithValue(value?: Date) {
+  function dateSelected(value?: Date) {
     setDateEntered(value ?? null);
-    setShowModal(false);
   }
 
   return (
     <div className="h-full flex flex-col">
-      <Header startHereClicked={openModal} />
+      <Header dateSelected={dateSelected} />
       <main className="flex-grow">
-      <DateDialog
-        show={showModal}
-        closeModal={closeModal}
-        closeModalWithValue={closeModalWithValue}
-      />
+      </main>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,52 @@
 import { Button } from "@headlessui/react";
 
 import timeCapsuleLogo from "../assets/logo.svg";
+import { DateDialog } from "./DateDialog";
+import { useState } from "react";
 
 interface HeaderProps {
-  startHereClicked: () => void;
+  dateSelected: (value?: Date) => void;
 }
 
-export const Header = ({ startHereClicked }: HeaderProps) => (
-  <header className="flex justify-between items-center bg-neutral-100 p-[3vw] border-solid border-black border-b-[0.5vw]">
-    <div className="w-1/2 flex flex-col items-center">
-      <img className="w-[40vw]" src={timeCapsuleLogo} alt="Time Capsule Logo" />
-      <p className="font-montserrat font-light text-[2.5vw] text-coldGray">
-        Unveiling your birthday wonders!
-      </p>
-    </div>
-    <Button
-      onClick={startHereClicked}
-      className="mr-[2vw] w-[25vw] h-[6vw] rounded-[1vw] text-[3vw] border-solid border-neutral-600 border-[0.15vw] text-coldGray font-montserrat focus:outline-none data-[hover]:bg-black/10"
-    >
-      Start Here!
-    </Button>
-  </header>
-);
+export const Header = ({ dateSelected }: HeaderProps) => {
+  const [showModal, setShowModal] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  function closeModalWithValue(value?: Date) {
+    dateSelected(value);
+    setShowModal(false);
+  }
+
+  return (
+    <header className="flex justify-between items-center bg-neutral-100 p-[3vw] border-solid border-black border-b">
+      <div className="w-1/2 flex flex-col items-center">
+        <img
+          className="w-[40vw]"
+          src={timeCapsuleLogo}
+          alt="Time Capsule Logo"
+        />
+        <p className="font-montserrat font-light text-[2.5vw] text-coldGray">
+          Unveiling your birthday wonders!
+        </p>
+      </div>
+      <Button
+        onClick={openModal}
+        className="mr-[2vw] w-[25vw] h-[6vw] rounded-small text-[3vw] border-solid border-neutral-600 border-thin text-coldGray font-montserrat focus:outline-none data-[hover]:bg-black/10"
+      >
+        Start Here!
+      </Button>
+      <DateDialog
+        show={showModal}
+        closeModal={closeModal}
+        closeModalWithValue={closeModalWithValue}
+      />
+    </header>
+  );
+};
