5b752172fee8f9db1fe9d8ea3a57c1cece46d724
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-10-03 22:38:56 -0400

    adding basic functions

--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
+
+// styles
+import styles from "./DemoPage.module.css";
+
+// components
 import { ControlPanelBackground } from "../components/ControlPanelBackground";
 import { DatePicker } from "../components/DatePicker";
 import { Door } from "../components/Door";
 import { GoButton } from "../components/GoButton";
 import { NamePlate } from "../components/NamePlate";
 import { RandomButton } from "../components/RandomButton";
-import styles from "./DemoPage.module.css";
+import { useNavigate } from "react-router-dom";
+
+const MIN_DATE = new Date(1950, 0, 1);
+const MAX_DATE = new Date();
 
 export const DemoPage = () => {
+  const [date, setDate] = useState(new Date(1955, 10, 5));
+  const navigate = useNavigate();
+
+  const updateDate = (date: Date) => {
+    setDate(date);
+  };
+
+  const goToSelectedDate = () => {
+    doorChime.play().then(() => {
+      navigate(generatePath(date));
+    });
+  };
+
+  const generatePath = (date: Date) => {
+    return `/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+  };
+
+  const pickRandomDate = () => {
+    const randomTime =
+      MIN_DATE.getTime() +
+      Math.random() * (MAX_DATE.getTime() - MIN_DATE.getTime());
+    updateDate(new Date(randomTime));
+  };
+
+  const doorChime = new Audio("doorbell.wav");
+
   return (
     <>
       <div className={styles.container}>
@@ -16,13 +51,13 @@ export const DemoPage = () => {
               <NamePlate />
             </div>
             <div className={styles.goButton}>
-              <GoButton onClick={() => window.alert("That's heavy")} />
+              <GoButton onClick={() => goToSelectedDate()} />
             </div>
             <div className={styles.datePicker}>
-              <DatePicker />
+              <DatePicker date={date} updateDate={updateDate} />
             </div>
             <div className={styles.randomButton}>
-              <RandomButton onClick={() => window.alert("Random date")} />
+              <RandomButton onClick={() => pickRandomDate()} />
             </div>
           </div>
         </div>
