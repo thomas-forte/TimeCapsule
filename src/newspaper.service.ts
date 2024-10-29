8dfab28173a1010daa4a7a03b684873674c55ab8
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-10-28 23:54:32 -0400

    fixed 1950 bad date formatting

--- src/newspaper.service.ts
+++ src/newspaper.service.ts
@@ -1,16 +1,9 @@
 import axios from "axios";
 
-const UNDERSCORE_DATES = new Date(1950, 6, 1);
-
 export const checkForNewspaper = async (
   date: Date,
   decade: string
 ): Promise<string | null> => {
-  let formattedDate = date.toISOString().substring(0, 10);
-  if (date < UNDERSCORE_DATES) {
-    formattedDate = formattedDate.replace(/-/g, "_");
-  }
-
   const url =
     "https://timecapsule.brendantrepal.com/images/newspapers" +
     `/${decade}/${date.getFullYear()}` +
@@ -19,7 +12,7 @@ export const checkForNewspaper = async (
       .padStart(2, "0")}-${date.toLocaleString("default", {
       month: "long",
     })}` +
-    `/${formattedDate}.jpg`;
+    `/${date.toISOString().substring(0, 10)}.jpg`;
 
   return await axios
     .get(url)
