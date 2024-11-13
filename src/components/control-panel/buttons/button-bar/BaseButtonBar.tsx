fd4ffb087bc7cf6f880f082da4f653d37761aa67
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-11-13 00:35:36 -0500

    the key prop is reserved, renamed

--- src/components/control-panel/buttons/button-bar/BaseButtonBar.tsx
+++ src/components/control-panel/buttons/button-bar/BaseButtonBar.tsx
@@ -3,7 +3,7 @@ import styles from "./BaseButtonBar.module.css";
 import classNames from "classnames";
 
 interface BaseButtonBarProps extends PropsWithChildren {
-  key: string;
+  name: string;
   active: boolean;
   onClick?: () => void;
 }
@@ -14,7 +14,7 @@ export interface BaseButtonBarChildProps {
 }
 
 export const BaseButtonBar = ({
-  key,
+  name,
   active,
   onClick,
   children,
@@ -36,7 +36,7 @@ export const BaseButtonBar = ({
   >
     <path
       d="M62,15.5L62,46.5C62,55.055 55.055,62 46.5,62L15.5,62C6.945,62 -0,55.055 -0,46.5L-0,15.5C-0,6.945 6.945,0 15.5,0L46.5,0C55.055,0 62,6.945 62,15.5Z"
-      fill={`url(#_${key}_Linear1)`}
+      fill={`url(#_${name}_Linear1)`}
     />
     <path
       d="M59,17L59,45C59,52.727 52.727,59 45,59L17,59C9.273,59 3,52.727 3,45L3,17C3,9.273 9.273,3 17,3L45,3C52.727,3 59,9.273 59,17Z"
@@ -44,7 +44,7 @@ export const BaseButtonBar = ({
     />
     <path
       d="M57,18L57,44C57,51.175 51.175,57 44,57L18,57C10.825,57 5,51.175 5,44L5,18C5,10.825 10.825,5 18,5L44,5C51.175,5 57,10.825 57,18Z"
-      fill={`url(#_${key}_Linear2)`}
+      fill={`url(#_${name}_Linear2)`}
     />
     <path
       d="M52,20.5L52,41.5C52,47.295 47.295,52 41.5,52L20.5,52C14.705,52 10,47.295 10,41.5L10,20.5C10,14.705 14.705,10 20.5,10L41.5,10C47.295,10 52,14.705 52,20.5Z"
@@ -53,7 +53,7 @@ export const BaseButtonBar = ({
     {children}
     <defs>
       <linearGradient
-        id={`_${key}_Linear1`}
+        id={`_${name}_Linear1`}
         x1="0"
         y1="0"
         x2="1"
@@ -67,7 +67,7 @@ export const BaseButtonBar = ({
         <stop offset="1" stopColor="#808a8c" stopOpacity="1" />
       </linearGradient>
       <linearGradient
-        id={`_${key}_Linear2`}
+        id={`_${name}_Linear2`}
         x1="0"
         y1="0"
         x2="1"
