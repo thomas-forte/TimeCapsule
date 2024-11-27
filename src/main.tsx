dd20ff68045c809876d6329b59545a82be9a8657
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-11-27 00:50:51 -0500

    switching to hosted fonts

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,38 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "@fontsource/oxanium/400.css";
 
+// 1950s
+import "@fontsource/carter-one/400.css";
+import "@fontsource/truculenta/400.css";
+
+// 1960s
+import "@fontsource/spicy-rice/400.css";
+import "@fontsource/life-savers/700.css";
+
+// 1970s
+import "@fontsource/bungee-shade/400.css";
+import "@fontsource/michroma/400.css";
+
+// 1980s
+import "@fontsource/monoton/400.css";
+import "@fontsource/orbitron/400.css";
+
+// 1990s
+import "@fontsource/gluten/400.css";
+import "@fontsource/comic-neue/400.css";
+
+// 2000s
+import "@fontsource/cute-font/400.css";
+import "@fontsource/jura/400.css";
+
+// 2010s
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/lato/400.css";
+
+// 2020s
+import "@fontsource/poppins/400.css";
+import "@fontsource/montserrat/400.css";
+
 import { App } from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
