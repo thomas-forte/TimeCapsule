0df7c28bf77a1e6e4e9ecb7b5fd7c7a06428f84f
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-02-19 00:28:08 -0500

    close button on zoom dialog

--- src/components/ZoomDialog.tsx
+++ src/components/ZoomDialog.tsx
@@ -1,4 +1,5 @@
 import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/16/solid";
 
 interface ZoomDialogProps {
   isOpen: boolean;
@@ -23,9 +24,17 @@ export const ZoomDialog = ({
       <div className="flex w-dvw h-dvh items-center justify-center p-4">
         <DialogPanel
           transition
-          className="rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+          className="rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 text-right"
         >
-          <div className="max-h-[90dvh] overflow-y-scroll">
+          <button
+            type="button"
+            className="inline-flex items-center gap-x-1.5 rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-white/20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 mb-3"
+            onClick={onClose}
+          >
+            <XMarkIcon className="-mr-0.5 size-5" />
+            Close
+          </button>
+          <div className="max-h-[85dvh] overflow-y-scroll">
             <img className="max-w-[90dvw]" src={imgSrc} alt={imgAlt} />
           </div>
         </DialogPanel>
