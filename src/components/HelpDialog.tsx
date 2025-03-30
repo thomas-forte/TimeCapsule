484e0ee97894ab8f1a7a7d6c233bdfacf51fb599
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-03-30 01:30:57 -0400

    help changes

--- src/components/HelpDialog.tsx
+++ src/components/HelpDialog.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 
 interface HelpDialogProps {
   isOpen: boolean;
@@ -50,7 +51,7 @@ export const HelpScreen = ({ isOpen, onClose }: HelpDialogProps) => {
     <Dialog open={isOpen} onClose={() => onClose()} className="relative z-50">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+        className="fixed inset-0 bg-black/65 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
       />
 
       <div className="fixed inset-0 z-10 w-dvw">
@@ -59,30 +60,42 @@ export const HelpScreen = ({ isOpen, onClose }: HelpDialogProps) => {
             transition
             className="relative h-dvh flex flex-col justify-between items-center transform overflow-hidden transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
-            <div className="text-[10dvh] font-montserrat">Help</div>
-            <div className="h-[70dvh] w-[80dvw] ms-[20dvw] text-[4dvh] font-montserrat">
+            <div className="text-[10dvh] font-montserrat text-white">Help</div>
+            <div className="h-[70dvh] w-[80dvw] ms-[20dvw] text-[4dvh] font-montserrat text-white">
               {getHelpPage(page)}
-              <button
-                type="button"
-                data-autofocus
-                onClick={() => onClose()}
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-              >
-                Close
-              </button>
             </div>
-            <div className="h-[10dvh] w-full flex justify-around items-center text-[4dvh] font-montserrat px-[20dvw]">
+            <div className="h-[10dvh] w-full flex justify-around items-center text-[4dvh] font-montserrat text-white px-[20dvw]">
               <div
                 className="cursor-pointer select-none min-w-[20dvw]"
                 onClick={() => setPage(page - 1)}
               >
-                {page != 0 ? "<- Previous" : ""}
+                {page != 0 ? (
+                  <div className="flex items-center">
+                    <ArrowLeftIcon className="size-10" />
+                    Previous
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div
+                className="cursor-pointer select-none min-w-[20dvw]"
+                onClick={() => onClose()}
+              >
+                Close
               </div>
               <div
                 className="cursor-pointer select-none min-w-[20dvw]"
                 onClick={() => setPage(page + 1)}
               >
-                {page != 2 ? "Next ->" : ""}
+                {page != 2 ? (
+                  <div className="flex items-center">
+                    Next
+                    <ArrowRightIcon className="size-10" />
+                  </div>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           </DialogPanel>
